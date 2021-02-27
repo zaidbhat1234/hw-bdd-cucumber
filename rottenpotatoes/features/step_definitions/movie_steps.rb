4e4b5fb8a12cbfb51ef6d40eea58297998bdd187
EC2 Default User <ec2-user@ip-172-31-89-192.ec2.internal>
2021-02-27 00:28:44 +0000

    Initial commit

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,6 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    
   end
   fail "Unimplemented"
 end
